3ef66d73241aacc862bc64165a42c368b8ef81e1
hyunkyuchoi <chlgusrb6@gmail.com>
2018-03-27 11:02:05 +0900

    'Javascript_2018-1_Test.js'

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,4 @@
 
-var myHeading = document.querySelector('h1');
-myHeading.innerHTML = 'Cloude Service<br> Hello Choi';
-var text = '최현규의 Cloude Service';
-var aa = 10.5;
-var a = 33.7;
-var b = a*aa;
-var iceCream = 'chocolate';
-
 var id = prompt('ID 입력:','');
 var ps = prompt('PS 입력:','');
 
@@ -14,7 +6,18 @@ if(ps === '940208')
 {
     alert('안녕하세요 최현규님 환영합니다.')
     alert(id + '가 로그인함.');
+    var myHeading = document.querySelector('h1');
+    myHeading.innerHTML = 'Cloude Service<br> Hello Choi';
+    var text = '최현규의 Cloude Service';
+    var aa = 10.5;
+    var a = 33.7;
+    var b = a*aa;
+    var iceCream = 'chocolate';
+}
+else if(ps==='')
+{
+    alert('비밀번호를 입력하세요.');
 }
 else{
-    alert('비밀번호가 틀렸습니다.')
+    alert('비밀번호가 틀렸습니다.');
 }
